2816fb352ef995c07eb996f73c767ebcfcedb250
Hemanth Bhushan <hemanth.bhushan@antiersolutions.com>
2023-04-18 09:54:47 +0530

    commit message

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,26 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
+
 fn main() {
     println!("Guess the number ");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
     println!("Enter the number");
 
-    let mut str = String::new();
+    let mut guess = String::new();
 
     io::stdin()
-        .read_line(&mut str)
+        .read_line(&mut guess)
         .expect("Failed to read line");
-    println!("You guessed: {str}");
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+    println!("You guessed: {guess}");
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
+    println!("the secret number is {secret_number}");
 }
